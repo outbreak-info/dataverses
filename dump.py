5dcfa71c4be8a6580f9efdb15e25c73bcd4b9ca6
timothymullen <timothy.liam.mullen@gmail.com>
2020-06-09 13:36:01 -0400

    covid_dataverse to dataverses

--- dump.py
+++ dump.py
@@ -6,7 +6,7 @@ biothings.config_for_app(config)
 import biothings.hub.dataload.dumper
 
 class DataverseDumper(biothings.hub.dataload.dumper.DummyDumper):
-    SRC_NAME = "covid_dataverse"
+    SRC_NAME = "dataverses"
     SRC_ROOT_FOLDER = os.path.join(config.DATA_ARCHIVE_ROOT, SRC_NAME)
 
     SCHEDULE = None
