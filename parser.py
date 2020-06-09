4ef7b4561318bacf9145cab34caaf0790dde776f
timothymullen <timothy.liam.mullen@gmail.com>
2020-06-09 17:45:10 -0400

    remove logging import

--- parser.py
+++ parser.py
@@ -4,12 +4,6 @@ import json
 from datetime    import date
 from html.parser import HTMLParser
 
-try:
-    from biothings import config
-    logging = config.logger
-except ImportError:
-    import logging
-    logging.basicConfig(filename="dataverselog.log", level=logging.INFO)
 
 QUERIES = ["2019-nCoV", "COVID-19", "COVID-19 virus", "COVID19", "COVID19 virus", "HCoV-19", "HCoV19", "Human coronavirus 19", "Human coronavirus 2019", "SARS-2", "SARS-CoV-2", "SARS-CoV2", "SARS2", "SARSCoV-2", "SARSCoV2",
                  "Severe acute respiratory syndrome coronavirus 2", "Wuhan coronavirus", "Wuhan seafood market pneumonia virus", "coronavirus disease", "coronavirus disease 19", "coronavirus disease 2019", "novel coronavirus", "novel coronavirus 2019"]
